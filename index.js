bf15e117219925bf0c5d1ff578a3bc35624887bb
Grant Bruemmer <grantbruemmer@gmail.com>
2023-06-15 15:48:53 -0500

    This actually works just fine but wasnt written in a way npm test liked

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Add your code here
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+// }
+//  const configurationObject = {
+//     method: "Post",
+//     headers: {
+//     "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({formData}),
+// }
+// fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(res => res.json())
+//     .then((obj) => console.log(obj))
+//     .catch(function err() {
+//         alert("Bad things! Ragnarok!")
+//         console.log(err.message)
+//     })
+const userInfo = {
+    name: "(Whatever Would Be Submitted)",
+    email: "(Whatever Would Be Submitted)",
+}
+const document.querySelector
+const configObj = {
+    method: "Post",
+    headers: {
+        "Content-Type": "application/json"
+    },
+        body: JSON.stringify({userInfo})
+}
+function submitData() {
+    fetch("http://localhost:3000/users", configObj)
+    .then(res => res.json())
+    .then((obj) => console.log(obj))
+    .catch(function err() {
+        alert("HEAL BOOTSTRAP!")
+        console.log(err.message)
+    return fetch("http://localhost:3000/users");
+    })
+}
